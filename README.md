4e9169c0d3f2e352adcd80972fcdc89ee454bd13
natodemon <aes17optimus@gmail.com>
2022-06-04 13:52:44 +0200

    Define user class, inital readme comments & datastore init

--- README.md
+++ README.md
@@ -6,4 +6,16 @@ The purpose of this project is to create a simple program that exposes and API w
 
 ## Implementation
 
-The program will be written in Python, a TDB database used to store data delivered through the API 
+Introduce this section about implement. and choices
+
+### Server-side
+
+Python due to familiarity, easy and speed of development & addequate application
+
+### Datastore
+
+DynamoDB due to easy of implementation, compatibility with Python and availability / reliability. For simple application allows DB management to be abstracted away and simply focus on development. Not cloud-agnostic but could easily be replaced with GCP firestore
+
+### Containerisation
+
+Docker.
--- birthdayApi/birthdayApi.py
+++ birthdayApi/birthdayApi.py
@@ -1,6 +1,21 @@
 from flask import Flask, jsonify
+import boto3
+from datetime import datetime, timedelta
+
+
+DB_DICT = {}
+
+class User:
+    def __init__(self, username, dob):
+        self.username = username
+        self.dob = dob
+
+    @classmethod
+    def fromString(cls, username, dob_string):
+        return cls(username, datetime.strptime(dob_string, '%Y-%m-%d'))
 
 app = Flask(__name__)
+db = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
 
 user = 'test_user'
 dob = '12/04/1993'
@@ -11,3 +26,8 @@ def dob_test():
     temp_dict['message'] = f'Hello {user}, your bday is {dob}'
 
     return jsonify(temp_dict)
+
+def db_write(user: User):
+    d
+def db_read() -> User:
+    a
