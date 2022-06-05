f01d35986706a2a6cb741e5a2c235de3afecc4c9
natodemon <aes17optimus@gmail.com>
2022-06-05 18:34:20 +0200

    Sanitise username input & except queries for non-existent users

--- birthdayApi/birthdayApi.py
+++ birthdayApi/birthdayApi.py
@@ -29,7 +29,7 @@ class User:
         if today.date() > temp_bday.date():
             temp_bday = self.dob.replace(year=(cur_year+1))
 
-        bday_delta = temp_bday - today
+        bday_delta = temp_bday.date() - today.date()
         return bday_delta.days
 
 
@@ -40,7 +40,8 @@ app = Flask(__name__)
 
 @app.route('/hello/<username>', methods=['PUT'])
 def updateUser(username):
-    clean_usr = username # Replace w/ escape(username) to string
+    username = escape(username)
+    clean_usr = str(username) # Replace w/ escape(username) to string
     usr_input = request.json
     usr = User.fromString(clean_usr, usr_input['dateOfBirth'])
     db_write(usr)
@@ -50,14 +51,21 @@ def updateUser(username):
 
 @app.route('/hello/<username>', methods=['GET'])
 def getBirthday(username):
-    clean_usr = username # Fix this
-    usr_obj = db_read(clean_usr)
-    bday_delta = usr_obj.daysToBday(datetime.now())
-    if bday_delta < 1:
-        msg = {"message": f"Hello, {clean_usr}! Happy birthday!"}
-    else:
-        msg = {"message": f"Hello, {clean_usr}! Your birthday is in {bday_delta} day(s)"}
-    return jsonify(msg), 200
+    username = escape(username)
+    clean_usr = str(username) # Fix this
+    try:
+        usr_obj = db_read(clean_usr)
+        bday_delta = usr_obj.daysToBday(datetime.now())
+        if bday_delta < 1:
+            msg = {"message": f"Hello, {clean_usr}! Happy birthday!"}
+        else:
+            msg = {"message": f"Hello, {clean_usr}! Your birthday is in {bday_delta} day(s)"}
+        statusCode = 200
+    except KeyError:
+        msg = {"message": f"Hello {clean_usr}, we do not seem to have your date of birth on record"}
+        statusCode = 404
+    return jsonify(msg), statusCode
+
 
 
 # Temporary methods for local key-value store
@@ -66,4 +74,7 @@ def db_write(user: User):
     DB_DICT[(usr_dict['username'])] = usr_dict['dob']
     
 def db_read(username) -> User:
-    return User.fromIsoformat(username, DB_DICT[username])
+    if username in DB_DICT:
+        return User.fromIsoformat(username, DB_DICT[username])
+    else:
+        raise KeyError(f'User {username} not found in the DB')
--- birthdayApi/dateTest.py
+++ birthdayApi/dateTest.py
@@ -1,9 +1,11 @@
 from datetime import date,datetime
 
-date_exp = '2022-05-01'
+date_0 = '2022-02-17'
+today = datetime.now()
 
-date_stamp = datetime.strptime(date_exp, '%Y-%m-%d').date()
+date_stamp = datetime.strptime(date_0, '%Y-%m-%d')
 
-date_string = date_stamp.isoformat()
+delta = date_stamp.replace(year=2023).date() - today.date()
+delta_1 = today.date() - date_stamp.replace(year=2023).date()
 
-print(date_string)
+print(delta)
